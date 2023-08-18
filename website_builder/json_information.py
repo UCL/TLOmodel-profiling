89a1e74bc058df807f4fd8febf127b3252627926
willGraham01 <1willgraham@gmail.com>
2023-08-18 15:15:22 +0100

    Document json_information

--- website_builder/json_information.py
+++ website_builder/json_information.py
@@ -12,12 +12,14 @@ STATS_COLUMNS = []
 
 def read_profiling_json(json_in: Path) -> Tuple[datetime, float]:
     """
-    Read the provided json file, which is a rendered output of a pyis session,
-    and return the JSON_COLUMNS data that stored within it.
+    Read the provided json file, and return the JSON_COLUMNS data stored within in.
+    The input json file should be a rendered output of a pyis session.
 
     Values are returned in the order that the JSON_COLUMNS variable gives their names.
+    If values cannot be found, defaults are assigned (usually None to flag missing data).
 
-    If values cannot be found, defaults (usually None to flag missing data) are assigned.
+    :param json_in: json file that is the rendered output of a .pyisession file.
+    :returns: A tuple of values that correspond to the values in JSON_COLUMNS, extracted from the input file.
     """
     # Pre-emptively set outputs to None in case some keys cannot be found
     # We need a default for start_time as this will be the DataFrame index
@@ -39,12 +41,13 @@ def read_profiling_json(json_in: Path) -> Tuple[datetime, float]:
 def read_additional_stats(stats_file: Path) -> None:
     """
     Read the provided file, which is assumed to be a file containing additional statistics
-    about the profiling run that cannot be conveyed by the pyis session file.
-
+    about a profiling run that cannot be conveyed by the pyis session file.
     Files recording additional statistics are assumed to be (able to be parsed as) json files.
 
     Values are returned in the order that the STATS_COLUMNS variable gives their names.
+    If values cannot be found, defaults are assigned (usually None to flag missing data).
 
-    If values cannot be found, defaults (usually None to flag missing data) are assigned.
+    :param json_in: A json-readable file containing statistics from a .pyisession.
+    :returns: A tuple of values that correspond to the values in STATS_COLUMNS, extracted from the input file.
     """
     return
