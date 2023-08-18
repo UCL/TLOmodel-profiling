99ec638a1c678e37fc73b8564eeeceb761aa2d1e
willGraham01 <1willgraham@gmail.com>
2023-08-18 10:26:28 +0100

    Rename src directory to better describe what it contains

--- website_build/_paths.py
+++ website_build/_paths.py
@@ -11,7 +11,7 @@ GIT_ROOT = (LOCATION_OF_THIS_FILE / "..").resolve()
 
 DEFAULT_BUILD_DIR = (LOCATION_OF_THIS_FILE / ".." / "build").resolve()
 
-SRC_DIR = (GIT_ROOT / "src").resolve()
+SRC_DIR = (GIT_ROOT / "profiling_pages").resolve()
 PROFILING_LOOKUP_TEMPLATE = (SRC_DIR / "profiling_index.md").resolve()
 INDEX_PAGE = (SRC_DIR / "index.md").resolve()
 RUN_STATS_LOOKUP_TEMPLATE = (SRC_DIR / "run_statistics.md").resolve()
